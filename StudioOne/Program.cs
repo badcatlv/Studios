024aa29fe5c6554acb84f2afe1726bd1f60b2912
Colleen Blake <colleenemilyblake@gmail.com>
2023-08-09 19:57:11 -0500

    Finished studion, without bonus yet

--- StudioOne/Program.cs
+++ StudioOne/Program.cs
@@ -1,33 +1,29 @@
 ﻿Console.WriteLine("Enter a radius: ");
 double r;
 double area;
-
+double circum;
+//return the area of a circle
 r = double.Parse(Console.ReadLine());
 Console.WriteLine("You entered " + r);
 area = Math.Pow(r, 2.0) * Math.PI;
+area = Math.Round(area, 3);
 Console.WriteLine("The area of a circle of radius " + r +": " + area);
+//return the circumference of the same circle
+circum = Math.PI * 2 * r;
+circum = Math.Round(circum, 3);
+Console.WriteLine("THe circumference is " +  circum);
 
-/*  Things to consider---
- *  What data type should the radius be? 
- *  What is the best way to get user input into a variable radius of that type?
- *  A = pi * r * r
- *  pi = 3.14
- *  r = radius
- *  
- *  use Math.PI and Math.Pow
- *  
- * Should result in---
- * Enter a radius: 2.5
- * The area of a circle of radius 2.5 is: 19.625
- * 
- * Using the same radius, calculate the circumference (2*pi*r) 
- * and diameter of the circle (2*r).
- * 
- * Ask the user for the miles per gallon of their car.
+
+/* Ask the user for the miles per gallon of their car.
  * If the radius that they entered is in miles, output how many gallons of gas 
- * they will use to go around this circle.
- * 
- * BONUS:
+ * they will use to go around this circle.*/
+Console.WriteLine("How many miles per gallons does your car get?");
+double gallons = double.Parse(Console.ReadLine());
+double milesPerCircum = gallons / circum;
+Console.WriteLine(milesPerCircum);
+
+
+ /* BONUS:
  * Think about how we could make this program more modular by 
  * breaking out some of the code into a separate class. 
  * For example, we could pull out the circle information into a 
