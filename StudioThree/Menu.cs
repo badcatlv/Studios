67f36f6c83b8db894bcea9049231db8f2c434464
Colleen Blake <colleenemilyblake@gmail.com>
2023-08-29 18:24:35 -0500

    what am i doing

--- StudioThree/Menu.cs
+++ StudioThree/Menu.cs
@@ -1,4 +1,6 @@
-﻿namespace StudioThreeRestaurantMenu
+﻿using StudioThreeRestaurantMenu;
+
+namespace StudioThreeRestaurantMenu
 {
     internal class Menu
 {
@@ -9,3 +11,14 @@
 
 }
 }
+
+//Fields:
+        //private List<MenuItem> menuItems: A list to store all the menu items in the menu.
+//Constructors:
+        //public Menu(): Initializes the menu with an empty list of menu items.
+//Methods:
+        //public void AddMenuItem(MenuItem menuItem): Adds a new menu item to the list of menu items.
+        //public void RemoveMenuItem(MenuItem menuItem): Removes a menu item from the list of menu items.
+        //public List<MenuItem> GetMenuItems(): Returns a list of all the menu items in the menu.
+        //public List<MenuItem> GetNewItems(): Returns a list of menu items that are marked as new.
+        //public List<MenuItem> GetItemsInCategory(string category): Returns a list of menu items in the specified category.
--- StudioThree/MenuItem.cs
+++ StudioThree/MenuItem.cs
@@ -1,12 +1,55 @@
 ﻿namespace StudioThreeRestaurantMenu
 {
     internal class MenuItem
-{
-    private string Item { get; set; }
-    private string Price { get; set; }
-    private string Description { get; set; }
-    private string Category { get; set; }
+    {
+        private string Item { get; set; }
+        private string Price { get; set; }
+        private string Description { get; set; }
+        private string Category { get; set; }
+        private bool IsNew { get; set; }
+        //private string NewItem { get; set; }
+        private DateTime LastUpdate { get; set; }
 
 
+        public MenuItem(string item, string price, string description, string category, bool isNew, DateTime lastUpdate)
+        {
+            Item = item;
+            Price = price;
+            Description = description;
+            Category = category;
+            IsNew = isNew;
+            LastUpdate = lastUpdate;
+            //IsNew.ToString();
+
+            Dictionary<string, string> singleMenuItem = new Dictionary<string, string>();
+            singleMenuItem.Add("Item", Item);
+            singleMenuItem.Add("Price", Price);
+            singleMenuItem.Add("Description", Description);
+            singleMenuItem.Add("New Item", IsNew);
+
+
+            //            Dictionary<string, string>  = new Dictionary<string,string>();
+
+            List<Dictionary<string, string>> MenuItems = new List<Dictionary<string, string>>();
+            MenuItems.Add()
+        }
+
+
+    }
 }
-}
+//Fields:
+//private string name: The name of the menu item.
+//private decimal price: The price of the menu item.
+//private string description: A brief description of the menu item.
+//private string category: The category of the menu item (appetizer, main course, dessert).
+//private bool isNew: Indicates whether the menu item is new.
+//private DateTime lastUpdated: Stores the date and time when the menu item was last updated.
+//Constructors:
+//public MenuItem(string name, decimal price, string description, string category):
+//Initializes the menu item with the provided name, price, description, and category.
+//Automatically sets isNew to true and lastUpdated to the current date and time.
+//Methods: (Add later, as needed.)
+//public void UpdateMenuItem(string name, decimal price, string description, string category):
+//Updates the menu item's details and sets isNew to true.
+//Updates the lastUpdated field to the current date and time.
+//public void MarkAsNotNew(): Sets isNew to false.
