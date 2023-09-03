19804ce11e6e01ce8ef67c3e9d0823ef8f040e0c
Colleen Blake <colleenemilyblake@gmail.com>
2023-09-03 12:00:31 -0500

    working thru menu classes

--- StudioThree/Menu.cs
+++ StudioThree/Menu.cs
@@ -1,24 +1,71 @@
-﻿using StudioThreeRestaurantMenu;
-
-namespace StudioThreeRestaurantMenu
+﻿namespace StudioThreeRestaurantMenu
 {
-    internal class Menu
-{
-    private string Item { get; set; }
-    private bool IsNew { get; set; } = false;
-    private string LastUpdate { get; set; }//use DateTime built in method
+    public class Menu
+    {
+        //private string Item { get; set; }
+        public List<string> MenuList { get; set; }
+        public DateTime LastUpdate { get; set; }//use DateTime built in method
+        public Menu(DateTime dateTime, List<string> items)
+        {
+            LastUpdate = dateTime;
+            MenuList = items;
+            PrintMenu(LastUpdate, MenuList);
+        }
+
+        public void PrintMenu(DateTime dateTime, List<string> strings) 
+        {
+            foreach (string item in strings)
+            {
+                int i = 0;
+                Console.WriteLine(i + item);
+                i++;
+                Console.WriteLine(dateTime);
+            }
+            Console.WriteLine(Environment.NewLine + dateTime);
+        }
+
+        //public void AddItem(Dictionary<string, string> menuItem)
+        //{
+        //    //List<Dictionary<string, string>> MenuList = new List<Dictionary<string, string>>();
+        //    if (!MenuList.Contains(menuItem))
+        //    {
+        //        MenuList.Add(menuItem);
+        //        LastUpdate = DateTime.Now;
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Item already on Menu.");
+        //    }
 
 
-}
+        //}
+
+        //public void RemoveItem(Dictionary<string, string> menuItem)
+        //{
+        //    //List<Dictionary<string, string>> MenuList = new List<Dictionary<string, string>>();
+        //    foreach (Dictionary<string, string> item in MenuList)
+        //    {
+        //        if (item.Equals(menuItem))
+        //        {
+        //            MenuList.Remove(menuItem);
+        //        }
+        //    }
+        //    if (MenuList.Contains(menuItem))
+        //    {
+        //        MenuList.Remove(menuItem);
+        //        LastUpdate = DateTime.Now;
+        //    }
+        //}
+    }
 }
 
 //Fields:
-        //private List<MenuItem> menuItems: A list to store all the menu items in the menu.
+//private List<MenuItem> menuItems: A list to store all the menu items in the menu.
 //Constructors:
-        //public Menu(): Initializes the menu with an empty list of menu items.
+//public Menu(): Initializes the menu with an empty list of menu items.
 //Methods:
-        //public void AddMenuItem(MenuItem menuItem): Adds a new menu item to the list of menu items.
-        //public void RemoveMenuItem(MenuItem menuItem): Removes a menu item from the list of menu items.
-        //public List<MenuItem> GetMenuItems(): Returns a list of all the menu items in the menu.
-        //public List<MenuItem> GetNewItems(): Returns a list of menu items that are marked as new.
-        //public List<MenuItem> GetItemsInCategory(string category): Returns a list of menu items in the specified category.
+//public void AddMenuItem(MenuItem menuItem): Adds a new menu item to the list of menu items.
+//public void RemoveMenuItem(MenuItem menuItem): Removes a menu item from the list of menu items.
+//public List<MenuItem> GetMenuItems(): Returns a list of all the menu items in the menu.
+//public List<MenuItem> GetNewItems(): Returns a list of menu items that are marked as new.
+//public List<MenuItem> GetItemsInCategory(string category): Returns a list of menu items in the specified category.
--- StudioThree/Program.cs
+++ StudioThree/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(DateTime.Now);
+using StudioThreeRestaurantMenu;
+
+List<string> list = new List<string>();
+MenuItem menuItem = new MenuItem();
+menuItem.AddMenuItem("cheese", 9.99, "appetizer", true, list);
+menuItem.AddMenuItem("bread", 5.99, "appetizer", false, list);
+menuItem.AddMenuItem("cheese", 9.99, "appetizer", true, list);
+menuItem.AddMenuItem("lobster", 29.99, "main course", true, list);
+menuItem.AddMenuItem("lobster", 29.99, "main course", false, list);
+
+Menu menu = new Menu(DateTime.Now, list);
+menu.PrintMenu(DateTime.Now, list);
