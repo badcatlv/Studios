4db3e01b316d53d42d81863c6e46094dc0bf1537
Colleen Blake <colleenemilyblake@gmail.com>
2023-09-04 14:17:00 -0500

    Working much better

--- StudioThree/Menu.cs
+++ StudioThree/Menu.cs
@@ -2,14 +2,14 @@
 {
     public class Menu
     {
-        private string Item { get; set; }
+        public string Item { get; set; }
         public List<MenuItem> MenuList { get; set; }
         public DateTime LastUpdate { get; set; }//use DateTime built in method
-        public Menu(string item, List<MenuItem> items)
+        public Menu(string item)
         {
             Item = item;
             LastUpdate = DateTime.Now;
-            MenuList = items;
+            MenuList = new List<MenuItem>();
             //PrintMenu(LastUpdate, MenuList);
         }
 
@@ -17,12 +17,28 @@
         {
             MenuList.Add(item);
         }
+        public void RemoveItem(MenuItem item)
+        {
+            if (MenuList.Contains(item))
+            {
+                MenuList.Remove(item);
+            }
+        }
+
+        public void PrintMenuItem(MenuItem menuItem)
+        {
+            // foreach (MenuItem item in MenuList)
+
+            Console.WriteLine("Item: " + menuItem.Name + "\nPrice: $" + menuItem.Price + "\nDescription: " + menuItem.Description + "\n");
+
+        }
 
         public void PrintMenu()
         {
             foreach (MenuItem item in MenuList)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(item.Name);
+
             }
             //Console.WriteLine(Environment.NewLine + LastUpdate);
         }
--- StudioThree/Program.cs
+++ StudioThree/Program.cs
@@ -1,9 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using StudioThreeRestaurantMenu;
 
-List<MenuItem> list = new List<MenuItem>();
 
-Menu menu = new Menu("Menu", list);
-MenuItem Burger = new MenuItem("burger", 8.49, "Quarter pound burger", "Main Course", true);
-menu.AddItem(Burger);
+Menu menu = new Menu("Menu");
+
+MenuItem Steak = new MenuItem("Steak", 15.59, "Fancy steak", "Main Course", true);
+
+Console.WriteLine(Steak.Name + "printed thru dot notation");
+Console.WriteLine(Steak.Price);
+Console.WriteLine(Steak.Description);
+Console.WriteLine(Steak.Category);
+
+MenuItem pumpkinPie = new MenuItem("Pumpkin Pie", 5.67, "Slice of pumpkin pie", "dessert", true);
+
+Console.WriteLine("\nadd items to menu list");
+menu.MenuList.Add(Steak);
+menu.AddItem(Steak);
+menu.AddItem(pumpkinPie);
+Console.WriteLine("\n" + menu.MenuList.Count + " print menu");
+menu.PrintMenu();
+Console.WriteLine("\ntest print item");
+menu.PrintMenuItem(Steak);
+
+Console.WriteLine("\nremove a steak");
+menu.RemoveItem(Steak);
+menu.PrintMenu();
+
+
+MenuItem nuggets = new MenuItem("Chicken Nuggets", 8.99, "Breaded and fried chicken nuggets", "appetizer", false);
+menu.MenuList.Add(nuggets);
+Console.WriteLine("\nWith nuggets\nItems on menu: " + menu.MenuList.Count);
 menu.PrintMenu();
